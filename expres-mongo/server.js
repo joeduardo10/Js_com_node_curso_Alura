3cb341c78489c98f683183d478f2110f71b77fd5
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-11-04 18:13:26 -0300

    "aula1"

---
+++ expres-mongo/server.js
@@ -0,0 +1,36 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de nodejs !!",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {"Content-Type":"text-plain"});
+    res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+    console.log("Servidor escutando...");
+})
+// import http from "http";
+
+// const PORT = 3000;
+
+// const rotas = {
+//   "/": "Curso de Express API",
+//   "/livros": "Entrei na rota livros",
+//   "/autores": "Entrei na rota autores"
+// };
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end(rotas[req.url]);
+// });
+
+// server.listen(PORT, () => {
+//   console.log("servidor escutando!");
+// });
